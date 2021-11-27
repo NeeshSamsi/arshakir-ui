f25b24c9f67e715d349729206238a438d1fd82ae
Neesh Samsi <avaneeshsamsi@gmail.com>
2021-11-27 15:15:41 +0530

    feat: web.dev colors config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,31 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "web.dev": {
+          dark: {
+            primary: "#313131",
+            secondary: "#514040",
+            "testimonial-primary": "#32435C",
+            "testimonial-secondary": "#6A635B",
+          },
+          light: "#F2F2F2",
+          accent: "#49AD09",
+          card: {
+            primary: colors.white,
+            secondary: "#EDEDED",
+          },
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
