c9505917106de651d5c69bcf4cda20457104d647
Neesh Samsi <avaneeshsamsi@gmail.com>
2021-11-28 15:32:41 +0530

    feat: semicircle asset & component

---
+++ components/webdev/Semicircle.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Semicircle = ({ w, h }) => {
+  return (
+    <svg width={w} height={h} viewBox="0 0 238 119" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M202.593 35.1736C224.676 57.2569 237.133 87.1664 237.265 118.383L0.501054 118.383C0.632724 87.1664 13.0904 57.2569 35.1736 35.1736C57.3748 12.9725 87.486 0.499998 118.883 0.5C150.28 0.500002 180.391 12.9725 202.593 35.1736Z"
+        stroke="#49AD09"
+      />
+    </svg>
+  );
+};
+
+export default Semicircle;
