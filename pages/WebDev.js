25992c82af4a225f4af647a37441353ab5defefd
Neesh <avaneeshsamsi@gmail.com>
2021-11-28 11:43:03 +0530

    feat: nav basic html

--- pages/WebDev.js
+++ pages/WebDev.js
@@ -1,8 +1,35 @@
+// NEXT
+import Link from "next/link";
+
+// COMPONENTS
+import CTA from "../components/webdev/CTA";
+
 export default function WebDev() {
   return (
     <>
-      <nav></nav>
-      <main>Web.Dev</main>
+      <nav>
+        <h1>Web.Dev</h1>
+        <div>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/">
+            <a>About</a>
+          </Link>
+          <Link href="/">
+            <a>Testimonials</a>
+          </Link>
+          <Link href="/">
+            <a>Contact</a>
+          </Link>
+          <Link href="/">
+            <a>Sign in</a>
+          </Link>
+
+          <CTA text="Sign up" />
+        </div>
+      </nav>
+      <main></main>
       <footer></footer>
     </>
   );
