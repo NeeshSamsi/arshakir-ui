34b0c995d43b090bff2f5e5877e852abbe2e2116
Neesh Samsi <avaneeshsamsi@gmail.com>
2021-11-28 15:32:21 +0530

    feat: cta styling

--- components/webdev/CTA.jsx
+++ components/webdev/CTA.jsx
@@ -1,3 +1,7 @@
 export default function CTA({ text }) {
-  return <a>{text}</a>;
+  return (
+    <a href="/" className="py-4 px-11 rounded-md bg-web.dev-accent text-white shadow-2xl">
+      {text}
+    </a>
+  );
 }
