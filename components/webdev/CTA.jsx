f46afe3ef67f74d5f2861df65093723d7fd0a62a
Neesh <avaneeshsamsi@gmail.com>
2021-11-29 20:18:36 +0530

    feat: CTA w custom prop styling

--- components/webdev/CTA.jsx
+++ components/webdev/CTA.jsx
@@ -1,7 +1,53 @@
-export default function CTA({ text }) {
-  return (
+import RightTriangle from "./RightTriangle";
+
+const BTN = ({ text, outline }) =>
+  outline ? (
+    <a
+      href="/"
+      className="py-4 px-11 rounded-md text-web.dev-dark-secondary border border-web.dev-dark-secondary shadow-2xl"
+    >
+      {text}
+    </a>
+  ) : (
     <a href="/" className="py-4 px-11 rounded-md bg-web.dev-accent text-white shadow-2xl">
       {text}
     </a>
   );
+
+const Link = ({ text, icon, theme }) => {
+  const color = theme.accent ? "accent" : "dark-secondary";
+  console.log("Link ", color);
+
+  return icon ? (
+    <div className="flex items-center space-x-3 rounded-md">
+      <a href="/" className={`py-4 font-medium text-lg text-web.dev-${color} ${theme.underline ? "underline" : ""}`}>
+        {text}
+      </a>
+      <div className="w-2">
+        <RightTriangle color={color} />
+      </div>
+    </div>
+  ) : (
+    <a href="/" className={`py-4 font-medium text-lg text-web.dev-${color} ${theme.underline ? "underline" : ""}`}>
+      {text}
+    </a>
+  );
+};
+
+export default function CTA({ text, icon, underline, accent, btn, outline }) {
+  const theme = { underline, accent };
+
+  if (btn) {
+    if (outline) {
+      return <BTN outline text={text} />;
+    }
+    return <BTN text={text} />;
+  }
+
+  if (!btn) {
+    if (icon) {
+      return <Link text={text} icon theme={theme} />;
+    }
+    return <Link text={text} theme={theme} />;
+  }
 }
