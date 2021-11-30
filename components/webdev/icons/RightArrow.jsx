52396dbd5e51260b700cfafd9d83cc61003ee51d
Neesh <avaneeshsamsi@gmail.com>
2021-11-30 18:07:30 +0530

    feat: left & right arrow icons

---
+++ components/webdev/icons/RightArrow.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function RightArrow() {
+  return (
+    <svg width="100%" height="100%" viewBox="0 0 20 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M20 4.99984L14.9713 0.833171L14.9713 4.1665L5.96244e-08 4.1665L3.97496e-08 5.83317L14.9713 5.83317L14.9713 9.1665L20 4.99984Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
