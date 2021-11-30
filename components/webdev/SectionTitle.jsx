a2b0dfcd0ff281fc6562d006b1baef444a1a9667
Neesh <avaneeshsamsi@gmail.com>
2021-11-30 13:37:36 +0530

    feat: SectionTitle component

---
+++ components/webdev/SectionTitle.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function SectionTitle({ title, subtitle }) {
+  return (
+    <div>
+      <div className="font-gilroy font-medium text-lg text-center">
+        <h4 className="py-8">{subtitle}</h4>
+        <h3 className="font-semibold text-5xl">{title}</h3>
+      </div>
+    </div>
+  );
+}
