de9d42be078103a85a975eec758571df07234099
Neesh <avaneeshsamsi@gmail.com>
2021-11-30 15:13:44 +0530

    fix: unnecessary py -> pb

--- components/webdev/SectionTitle.jsx
+++ components/webdev/SectionTitle.jsx
@@ -4,7 +4,7 @@ export default function SectionTitle({ title, subtitle }) {
   return (
     <div>
       <div className="font-gilroy font-medium text-lg text-center">
-        <h4 className="py-8">{subtitle}</h4>
+        <h4 className="pb-8">{subtitle}</h4>
         <h3 className="font-semibold text-5xl">{title}</h3>
       </div>
     </div>
