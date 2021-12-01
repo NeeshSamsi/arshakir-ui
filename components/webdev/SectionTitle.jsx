162389708e225e3961e06ccdafd6a37f7ea00e0f
Neesh <avaneeshsamsi@gmail.com>
2021-12-01 11:41:31 +0530

    feat: dynamic single title

--- components/webdev/SectionTitle.jsx
+++ components/webdev/SectionTitle.jsx
@@ -4,8 +4,9 @@ export default function SectionTitle({ title, subtitle }) {
   return (
     <div>
       <div className="font-gilroy font-medium text-lg text-center">
-        <h4 className="pb-8">{subtitle}</h4>
-        <h3 className="font-semibold text-5xl">{title}</h3>
+        {subtitle && <h4 className="pb-8">{subtitle}</h4>}
+
+        {title && <h3 className="font-semibold text-5xl">{title}</h3>}
       </div>
     </div>
   );
