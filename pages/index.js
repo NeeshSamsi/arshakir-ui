a3713423572996262886015b605124aaba928161
Neesh Samsi <avaneeshsamsi@gmail.com>
2021-12-08 14:37:29 +0530

    basic links to projects

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>Hello World</h1>
+      <Link href="/webdev">
+        <a>Web.Dev - Shopify Developer's Landing Page</a>
+      </Link>
+      <Link href="/jadoo">
+        <a>Jadoo - Travel Agency Landing Page</a>
+      </Link>
     </main>
   );
 }
